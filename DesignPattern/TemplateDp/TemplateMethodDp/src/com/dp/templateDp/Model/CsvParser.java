7b350ef602461496f959fcf283cc0415c930d143
Prathame123 <prathameshsomavanshi20@gmail.com>
2025-09-28 18:46:35 +0530

    Template Design Pattern

---
+++ DesignPattern/TemplateDp/TemplateMethodDp/src/com/dp/templateDp/Model/CsvParser.java
@@ -0,0 +1,14 @@
+package com.dp.templateDp.Model;
+
+public class CsvParser extends DataParser {
+
+	@Override
+	protected void ReadData() {
+		System.out.println("CsvParser.ReadData()");
+	}
+
+	@Override
+	protected void ProcessData() {
+		System.out.println("CsvParser.ProcessData()");
+	}
+}
---
+++ DesignPattern/TemplateDp/TemplateMethodDp/src/com/dp/templateDp/Model/ExcelParser.java
@@ -0,0 +1,14 @@
+package com.dp.templateDp.Model;
+
+public class ExcelParser extends DataParser {
+
+	@Override
+	protected void ReadData() {
+		System.out.println("ExcelParser.ReadData()");
+	}
+
+	@Override
+	protected void ProcessData() {
+		System.out.println("ExcelParser.ProcessData()");
+	}
+}
